1fe05f224089899fc80e500a714c92c32b4cc5eb
Vendeetta <7865330@gmail.com>
2022-12-19 17:34:30 +0300

    Initial commit

---
+++ src/main/java/com/example/JavaQuestionBot/JavaQuestionBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.JavaQuestionBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaQuestionBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaQuestionBotApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/JavaQuestionBot/service/TelegramBot.java
@@ -0,0 +1,64 @@
+package com.example.JavaQuestionBot.service;
+
+import com.example.JavaQuestionBot.config.BotConfig;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.List;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    private final BotConfig config;
+    private final static String UNKNOWN_COMMAND = "Извините, я не знаю такой команды.";
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (update.hasMessage() && message.hasText()) {
+            String messageText = message.getText();
+            long chatId = message.getChatId();
+            String userName = message.getChat().getFirstName();
+            switch (messageText) {
+                case "/start":
+                    startCommandReceived(chatId, userName);
+                    break;
+                default: sendMessage(chatId, UNKNOWN_COMMAND);
+            }
+        }
+    }
+
+    private void startCommandReceived(Long chatId, String userName) {
+        String answer = "Привет, " + userName + "!";
+        sendMessage(chatId, answer);
+    }
+
+    private void sendMessage (long chatId, String textToSend){
+        SendMessage messenger = new SendMessage();
+        messenger.setChatId(String.valueOf(chatId));
+        messenger.setText(textToSend);
+        try {
+            execute(messenger);
+        } catch (TelegramApiException e) {
+
+        }
+    }
+}
