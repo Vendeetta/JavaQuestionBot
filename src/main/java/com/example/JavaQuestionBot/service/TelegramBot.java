e347cbba0af1694faa38449e4237e760dd6cda3d
Vendeetta <7865330@gmail.com>
2022-12-22 17:32:23 +0300

    Changeback logback.xml and added menu button.

--- src/main/java/com/example/JavaQuestionBot/service/TelegramBot.java
+++ src/main/java/com/example/JavaQuestionBot/service/TelegramBot.java
@@ -4,11 +4,15 @@ import com.example.JavaQuestionBot.config.BotConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -18,9 +22,22 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     private final BotConfig config;
     private final static String UNKNOWN_COMMAND = "Извините, я не знаю такой команды.";
+    private final static String HELP_TEXT = "Данный бот написан для помощи в изучении Java.\n\n" +
+                                            "По команде /question бот выдает произвольный вопрос по Java.\n\n" +
+                                            "После ответа на вопрос, Вы можете посмотреть ответ, предлагаемый ботом.\n\n"+
+                                            "Продуктивного Вам обученияй! ;)";
 
     public TelegramBot(BotConfig config) {
         this.config = config;
+        List<BotCommand> commands = new ArrayList<>();
+        commands.add(new BotCommand("/start", "Запустить бота."));
+        commands.add(new BotCommand("/question", "Задать произвольный вопрос по Java"));
+        commands.add(new BotCommand("/help", "Получить справку по боту"));
+        try {
+            this.execute(new SetMyCommands(commands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Ошибка при установке меню бота :" + e.getMessage());
+        }
     }
 
     @Override
@@ -45,6 +62,9 @@ public class TelegramBot extends TelegramLongPollingBot {
                 case "/старт":
                     startCommandReceived(chatId, userName);
                     break;
+                case "/help":
+                    helpCommandReceived(chatId, userName);
+                    break;
                 default: sendMessage(chatId, UNKNOWN_COMMAND);
             }
         }
@@ -54,6 +74,11 @@ public class TelegramBot extends TelegramLongPollingBot {
         String answer = "Привет, " + userName + "!";
         log.info("Replied to " + userName);
         sendMessage(chatId, answer);
+    }
+
+    private void helpCommandReceived(Long chatId, String userName) {
+        log.info("HELP COMMAND: Replied to " + userName);
+        sendMessage(chatId, HELP_TEXT);
     }
 
     private void sendMessage (long chatId, String textToSend){
